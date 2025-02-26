b18531d42103066bbec5009f7d6de6ae77fc928f
Boluwatife Adeshina <bdadeshina1@gmail.com>
2025-02-25 23:57:24 -0500

    Reformatted code with prettier

--- web/components/Icon.tsx
+++ web/components/Icon.tsx
@@ -1,31 +1,44 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
-type IconProps = { name: string }
+type IconProps = { name: string };
 
 export default function Icon({ name }: IconProps) {
-    const [image, setImage] = useState<string | null>(null)
+  const [image, setImage] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchImage = async () => {
-            try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-                const data = await response.json()
-                const image = data.sprites.front_default
-                setImage(image)
-            } catch (e) {
-                console.log("Failed to fetch Pokémon image", e);
-            }
-        };
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${name}`
+        );
+        const data = await response.json();
+        const image = data.sprites.front_default;
+        setImage(image);
+      } catch (e) {
+        console.log('Failed to fetch Pokémon image', e);
+      }
+    };
 
-        fetchImage();
-    }, [name]);
+    fetchImage();
+  }, [name]);
 
-    if (!image) return <div className="flex items-center justify-center p-2 bg-slate-300 rounded-md w-60 h-60 shadow-[4px_4px_10px_rgba(0,0,0,0.25)] hover:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.25)] text-center text-gray-500">Loading...</div>;
-
+  if (!image)
     return (
-        <div className="flex items-center justify-center p-2 bg-slate-300 rounded-md w-60 h-60 shadow-[4px_4px_10px_rgba(0,0,0,0.25)] hover:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.25)]">
-          <Image src={image} alt={name} width={200} height={200} className="object-contain items-center"/>
-        </div>
-      );
-  }
+      <div className="flex items-center justify-center p-2 bg-slate-300 rounded-md w-60 h-60 shadow-[4px_4px_10px_rgba(0,0,0,0.25)] hover:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.25)] text-center text-gray-500">
+        Loading...
+      </div>
+    );
+
+  return (
+    <div className="flex items-center justify-center p-2 bg-slate-300 rounded-md w-60 h-60 shadow-[4px_4px_10px_rgba(0,0,0,0.25)] hover:shadow-[inset_4px_4px_10px_rgba(0,0,0,0.25)]">
+      <Image
+        src={image}
+        alt={name}
+        width={200}
+        height={200}
+        className="object-contain items-center"
+      />
+    </div>
+  );
+}
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -9,27 +9,29 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { MdCatchingPokemon } from "react-icons/md";
+import { MdCatchingPokemon } from 'react-icons/md';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const isPokemonPage = router.isReady && typeof router.query.pokemon === "string";
+  const isPokemonPage =
+    router.isReady && typeof router.query.pokemon === 'string';
 
   return (
-    <QueryClientProvider client={ queryClient }>
-      <div className={`flex ${ isPokemonPage ? "bg-white" : "bg-black" } w-screen h-20 justify-center items-center top-0 fixed`}>
+    <QueryClientProvider client={queryClient}>
+      <div
+        className={`flex ${isPokemonPage ? 'bg-white' : 'bg-black'} w-screen h-20 justify-center items-center top-0 fixed`}>
         <Link href="/">
-          <MdCatchingPokemon className={`transition-colors duration-500 ease-in-out size-20 hover:text-[#FF0000] ${ isPokemonPage ? "text-black" : ""}`}/>
+          <MdCatchingPokemon
+            className={`transition-colors duration-500 ease-in-out size-20 hover:text-[#FF0000] ${isPokemonPage ? 'text-black' : ''}`}
+          />
         </Link>
       </div>
 
-      <div className={`pt-20 ${ isPokemonPage ? "bg-black" : "bg-white"}`}>
-        <Component {...pageProps}/>
+      <div className={`pt-20 ${isPokemonPage ? 'bg-black' : 'bg-white'}`}>
+        <Component {...pageProps} />
       </div>
-      
-
     </QueryClientProvider>
   );
 }
